fb3da45d10270678776cbde183400c74720a8b13
Luis Ceballo <66170740+LuisCebNu@users.noreply.github.com>
2022-08-07 22:09:27 -0300

    Added STDIN support!

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use  colored::*;
 use structopt::StructOpt;
 use failure::ResultExt;
 use exitfailure::ExitFailure;
+use std::io::{self,Read};
 
 #[derive(StructOpt)]
 struct  Options{
@@ -18,6 +19,10 @@ struct  Options{
     #[structopt(short = "f", long="file", parse(from_os_str))]
     ///Load the dog picture from the specified file
     dogfile: Option<std::path::PathBuf>,
+
+    #[structopt(short = "i", long="stdin")]
+    ///Read the message from STDIN instead of the argument
+    stdin: bool,
 }
 
 //We return exitFailure instead of failure::Error
@@ -27,7 +32,12 @@ fn main() -> Result<(),ExitFailure> {
     // let message = std::env::args().nth(1)
     //     .expect("Missing the message. Usage: dogsay <message>");
     let options = Options::from_args(); //[2]
-    let message = options.message;
+    let mut message = String::new();
+    if options.stdin {
+        io::stdin().read_to_string(&mut message)?;
+    }else{
+        message = options.message;
+    }
     let eye = if options.dead {"X"} else {"O"};
     println!("{}",message.bright_yellow().underline().on_purple()); //<- we print the argument here
     match &options.dogfile {
